8a5e67cac0502f8be4b4445d67bb4c8c4c8ce965
hoanghao01 <haonguyen6345@gmail.com>
2024-03-20 21:49:51 +0700

    contacts sendmail

--- js/21810206.js
+++ js/21810206.js
@@ -1,4 +1,5 @@
 const API = 'https://web1-api.vercel.app/api'
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/user'
 
 async function loadData(request, templateId, viewId) {
     const response = await fetch(`${API}/${request}`);
@@ -10,4 +11,20 @@ async function loadData(request, templateId, viewId) {
     var view = document.getElementById(viewId)
     view.innerHTML = template(context);
 
+}
+
+async function getAuthenticateToken(username, password) {   
+    let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ username, password})
+    });
+    let result = response.json();
+    if (response.status === 200) {
+        return result.token;
+    }
+    throw new Error(result.message);
 }
