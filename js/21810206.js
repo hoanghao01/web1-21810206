3a3586c53213c41dd4b2fc2d23ca097f96bc148d
hoanghao01 <haonguyen6345@gmail.com>
2024-03-22 16:51:27 +0700

    Add comment

--- js/21810206.js
+++ js/21810206.js
@@ -1,5 +1,5 @@
 const API = 'https://web1-api.vercel.app/api'
-const AUTHENTICATE_API = 'https://web1-api.vercel.app/user'
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/users'
 
 async function loadData(request, templateId, viewId) {
     const response = await fetch(`${API}/${request}`);
@@ -11,7 +11,7 @@ async function loadData(request, templateId, viewId) {
     var view = document.getElementById(viewId)
     view.innerHTML = template(context);
 
-}
+ }
 
 async function getAuthenticateToken(username, password) {   
     let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
@@ -63,6 +63,11 @@ function displayControls(isLogin = true) {
 		linkLogins[i].style.display = displayLogin;
 		linkLogouts[i].style.display = displayLogout;
 	}
+
+    let leaveComment = document.getElementById('leave-comment');
+    if (leaveComment) {
+        leaveComment.style.display = displayLogout;
+    }
 
 }
 
